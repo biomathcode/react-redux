7c51db86382e9be3c1cdb0d1a4c2188ab1a92f68
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-04 00:48:35 +0530

    added the todo app

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo';
+
 class App extends Component{
   state = {
     todos: [
@@ -14,11 +16,19 @@ class App extends Component{
     });
     this.setState({todos}) 
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState(
+      {todos}
+    )
+  }
   render(){
     return(
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos deleteTodo ={this.deleteTodo}todos={this.state.todos}/>
+        <AddTodo addTodo= {this.addTodo}/>
       </div>
     )
   }
