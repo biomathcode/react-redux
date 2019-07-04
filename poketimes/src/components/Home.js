b7d770cc7214140f013bffd703eb3a60bc10ab03
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-04 13:07:03 +0530

    added axios for getting the data from a website

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
+class Home extends Component{
+    state ={
+        posts: [ ]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            console.log(res)
+            this.setState({
+                posts: res.data.slice(0,10)
+            })
+        })
 
-const Home = () => {
-    return (
+    }
+    render(){
+    const{posts} = this.state;
+    const postList = posts.length ? (
+        posts.map(post => {
+            return(
+                <div className="post card" key ={post.id}>
+                    <div className="card-container">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+
+                </div>
+            )
+        })
+    ): (
+        <div className='center'>No posts yet</div>
+    )
+        return (
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rem voluptas saepe cupiditate nemo iusto earum, dolorem numquam ea rerum ab fugit. Illo dolor laborum odit, excepturi velit minus modi!</p>
+           {postList}
         </div>
     )
+   }
+
 };
 
 export default Home;
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Link, NavLink, withRouter} from 'react-router-dom'
+import {Link,withRouter} from 'react-router-dom'
 
 
 const Navbar = (props) => {
-    console.log(props)
+    /* console.log(props)
     setTimeout(() => {
         props.history.push('/about')
-    }, 2000);
+    }, 2000) */;
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
