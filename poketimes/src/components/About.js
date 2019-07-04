2ef532579c99966f48cb6716af47379808f6ce88
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-04 06:18:24 +0530

    added routing

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rem voluptas saepe cupiditate nemo iusto earum, dolorem numquam ea rerum ab fugit. Illo dolor laborum odit, excepturi velit minus modi!</p>
+        </div>
+    )
+};
+
+export default About;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rem voluptas saepe cupiditate nemo iusto earum, dolorem numquam ea rerum ab fugit. Illo dolor laborum odit, excepturi velit minus modi!</p>
+        </div>
+    )
+};
+
+export default Home;
