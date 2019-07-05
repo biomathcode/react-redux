0a308cfcd2fcbaa93e1f6920b89b94ad22b1ec64
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-05 20:54:55 +0530

    added images to the website

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 class Home extends Component{
     state ={
         posts: [ ]
@@ -20,8 +22,11 @@ class Home extends Component{
         posts.map(post => {
             return(
                 <div className="post card" key ={post.id}>
+                    <img src={Pokeball} alt="A pokeball image"/>
                     <div className="card-container">
-                        <span className="card-title">{post.title}</span>
+                        <Link to={'/' + post.id}>
+                        <span className="card-title red-text">{post.title}</span>
+                        </Link>
                         <p>{post.body}</p>
                     </div>
 
@@ -32,7 +37,7 @@ class Home extends Component{
         <div className='center'>No posts yet</div>
     )
         return (
-        <div className="container">
+        <div className="container home">
             <h4 className="center">Home</h4>
            {postList}
         </div>
---
+++ poketimes/src/components/Post.js
@@ -0,0 +1,38 @@
+import React,{ Component} from 'react'
+import axios from 'axios'
+class Post extends Component {
+    state = {
+        post:null
+    }
+    componentDidMount(){
+        console.log(this.props);
+        let id = this.props.match.params.post_id;
+        axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
+        .then(res => {
+            this.setState({
+                post: res.data
+            })
+            
+        })
+        this.setState ({
+            id
+        })
+        
+    }
+    render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+            </div>
+        ):(
+            <div className='center'>Loading Post...</div>
+        )
+        return (
+            <div className="container">
+               {post}
+            </div>
+        )
+    }
+}
+export default Post;
