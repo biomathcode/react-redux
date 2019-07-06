8ba088c638b99dba8f49554d4242d3033fe27ae7
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-07 00:06:40 +0530

    added actions

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,11 +1,22 @@
 import React,{ Component} from 'react'
 import {connect} from 'react-redux'
+import {deletePost} from '../actions/postActions'
 class Post extends Component {
+    handleClick=()=> {
+        this.props.deletePost(this.props.post.id)
+        this.props.history.push('/')
+    }
     render() {
+        console.log(this.props)
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ):(
             <div className='center'>Loading Post...</div>
@@ -23,4 +34,12 @@ const mapStateToProps = (state, ownProps) => {
         post: state.posts.find(post => post.id === id)
     }
 }
-export default connect(mapStateToProps)(Post);
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => {dispatch(deletePost(id))}
+    }
+
+
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
