c2ff6c8ae6010a69f8888b0ba190687a59d60c14
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-12 13:29:09 +0530

    added dynamic date

--- marioplan/src/components/projects/ProjectSummary.js
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import moment from 'moment'
 const ProjectSummary = ({project}) =>{
     return(
          <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">{project.title}</span>
                     <p>{project.content}</p>
-                    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-                    <p className="grey-text">9th July, 4am</p>
+                    <p className="grey-text">Posted by {project.authorFirstName} {project.authorLastName}</p>
+                    <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
                 </div>
             </div>
     )
